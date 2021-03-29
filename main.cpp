a513a7b280af2393042f0712e63978be5a7b76bf
Alex <frstfng@gmail.com>
2021-03-29 20:04:09 +0300

    SimpleCode_73

--- main.cpp
+++ main.cpp
@@ -2,8 +2,31 @@
 
 using namespace std;
 
+class Object {
+public:
+  int pivotX;
+  int pivotY;
+  int pivotZ;
+};
+
 int main()
 {
-  cout << "Hello World!" << endl;
+  Object world;
+  Object object1;
+  int shiftX = 10;
+  int shiftY = 10;
+  int shiftZ = 10;
+
+  world.pivotX = 0;
+  world.pivotY = 1;
+  world.pivotZ = 0;
+
+  object1.pivotX = world.pivotX + shiftX;
+  object1.pivotY = world.pivotY + shiftY;
+  object1.pivotZ = world.pivotZ + shiftZ;
+
+  cout << world.pivotX << "|" << world.pivotY << "|" << world.pivotZ << endl;
+  cout << object1.pivotX << "|" << object1.pivotY << "|" << object1.pivotZ << endl;
+
   return 0;
 }
